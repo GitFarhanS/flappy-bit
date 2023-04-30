a4f89ff32ebad389b095978dd1a500d95a535fd7
Farhan Shaikh <34765527+GitFarhanS@users.noreply.github.com>
2023-04-30 12:46:51 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.AB, function () {
+    game.pause()
+})
+let empty_obstacle = 0
+let ticks = 0
+basic.showString("FlappyBit!")
+let bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
+let obstacles: game.LedSprite[] = []
+let index = 0
+basic.forever(function () {
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+        obstacles.removeAt(0).delete()
+    }
+    for (let obstacle of obstacles) {
+        obstacle.change(LedSpriteProperty.X, -1)
+    }
+    if (ticks % 3 == 0) {
+        empty_obstacle = randint(0, 4)
+        for (let index = 0; index <= 4; index++) {
+            if (index != empty_obstacle) {
+                obstacles.push(game.createSprite(4, index))
+            }
+        }
+    }
+    for (let obstacle of obstacles) {
+        if (obstacle.get(LedSpriteProperty.X) == bird.get(LedSpriteProperty.X) && obstacle.get(LedSpriteProperty.Y) == bird.get(LedSpriteProperty.Y)) {
+            game.gameOver()
+        }
+    }
+    ticks += 1
+    basic.pause(1000)
+})
+basic.forever(function () {
+    if (input.rotation(Rotation.Roll) < 0) {
+        bird.change(LedSpriteProperty.Y, input.rotation(Rotation.Roll) / 10 * -1)
+    } else if (input.rotation(Rotation.Roll) > 0) {
+        bird.change(LedSpriteProperty.Y, input.rotation(Rotation.Roll) / 10 * -1)
+    }
+})
